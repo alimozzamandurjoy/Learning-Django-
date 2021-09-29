df550ae53b769e6d847a4a7520d1b4f7d0d6f1ae
Durjoy <alimozzamandurjoy@gmail.com>
2021-09-29 21:33:38 +0600

    added comment

--- News Paper/newspaper_project/articles/admin.py
+++ News Paper/newspaper_project/articles/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Article
+from django.contrib.admin.options import StackedInline
+from django.db import models
 
-admin.site.register(Article)
+from .models import Article, Comment
+
+# Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model= Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines=[
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- News Paper/newspaper_project/articles/models.py
+++ News Paper/newspaper_project/articles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.db.models.base import Model
 from django.urls import reverse
 
 # Create your models here.
@@ -20,3 +21,18 @@ class Article(models.Model):
         return self.title
     def get_absolute_url(self):
         return reverse('article_detail', args=[str(self.id)])
+class Comment(models.Model):
+
+    article= models.ForeignKey(Article, on_delete=models.CASCADE,related_name='comments')
+    comment= models.CharField(max_length=140)
+    author= models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        
+    )
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('article_list')
